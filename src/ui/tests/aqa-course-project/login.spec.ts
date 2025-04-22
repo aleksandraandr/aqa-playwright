32555de479ae9b65be620a6c6f6e15b2a5209e81
aleksandraandr <ponytail1986@gmail.com>
2025-04-22 17:49:17 +0200

    Fixes based on review comments

--- src/ui/tests/aqa-course-project/login.spec.ts
+++ src/ui/tests/aqa-course-project/login.spec.ts
@@ -1,16 +1,32 @@
 import { test, expect } from "@playwright/test";
 
 test.describe('[UI] [aqa-course-project] Login', () => {
-    test('Login and UI verification', async ({ page }) => {
+    test('Login and UI verification + waitForFunction', async ({ page }) => {
         await page.goto('https://anatoly-karpovich.github.io/aqa-course-project/#');
         await page.getByLabel('Email address').fill('test@gmail.com');
         await page.getByLabel('Password').fill('12345678');
         await page.getByRole('button', { name: 'Login' }).click();
-        await page.waitForSelector('.spinner-border', { state: 'visible', timeout: 2000 });
-        await page.waitForSelector('.spinner-border', { state: 'detached' });
+        await page.waitForFunction(() => {
+            const spinners = document.querySelectorAll('.spinner-border');
+            return spinners.length === 0;
+          }, { timeout: 7000 });
         const userDisplay = page.getByText('Anatoly', { exact: false });
         await expect(userDisplay).toBeVisible();
         const sidebar = page.locator('#sidebar');
         await expect(sidebar).toHaveScreenshot('sidebar-home.png');
     });
+
+    test('Login and UI verification + toHaveCount', async ({ page }) => {
+        await page.goto('https://anatoly-karpovich.github.io/aqa-course-project/#');
+        await page.getByLabel('Email address').fill('test@gmail.com');
+        await page.getByLabel('Password').fill('12345678');
+        await page.getByRole('button', { name: 'Login' }).click();
+        const spinners = page.locator('.spinner-border');
+        await expect(spinners).toHaveCount(0, { timeout: 7000 });
+        const userDisplay = page.getByText('Anatoly', { exact: false });
+        await expect(userDisplay).toBeVisible();
+        const sidebar = page.locator('#sidebar');
+        await expect(sidebar).toHaveScreenshot('sidebar-home.png');
+    });
+
 });
